adf3f4bcf67f8b1cdb23a5080b90a847eb5dc923
arman aramesh <armanaramesh2020ne@gmail.com>
2023-04-11 11:38:46 +0430

    [update] levelmanager prototype and neccessary prefabs

---
+++ Assets/Scripts/ButtonLevelController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonLevelController : MonoBehaviour
+{
+    #region Public Variable
+    public Text txtLevelNumber;
+    public GameObject LockImage;
+    //public GameObject coinRepo;
+    public AudioSource audioSrc;
+    public AudioClip clipOpen;
+    public AudioClip clipLock;
+    //public CoinsRepository coinRepo;
+    #endregion
+
+    #region Private Variable
+    private bool Locked;
+    private int num;
+    //private CoinsRepository coinRepo;
+    #endregion
+
+    #region Public Method
+    public void Init(int number,bool isLocked)
+    {
+        Locked = isLocked;
+        if(isLocked)
+        {
+            LockImage.SetActive(true);
+            txtLevelNumber.gameObject.SetActive(false);
+        }
+        else
+        {
+            LockImage.SetActive(false);
+            txtLevelNumber.gameObject.SetActive(true);
+            txtLevelNumber.text = (number + 1).ToString();
+            num = number;
+        }
+    }
+
+    public void Click()
+    {
+        //coinRepo.Save();
+        if (Locked == false)
+        {
+            audioSrc.PlayOneShot(clipOpen);
+            //string LevelNumber = "Level " + num;
+            //SceneManager.LoadScene(LevelNumber);
+            StartCoroutine(AfterSound());
+        }
+        else
+        {
+            audioSrc.PlayOneShot(clipLock);
+        }
+    }
+    #endregion
+
+    #region Private Method
+
+    private void Start()
+    {
+        //coinRepo = FindObjectOfType<CoinsRepository>();
+    }
+    private IEnumerator AfterSound()
+    {
+        yield return new WaitForSeconds(clipOpen.length);
+        string LevelNumber = "Level" + num;
+        SceneManager.LoadScene(LevelNumber);
+    }
+
+    private void OnApplicationQuit()
+    {
+        //coinRepo.Save();
+    }
+    #endregion
+}
---
+++ Assets/Scripts/LevelGridController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelGridController : MonoBehaviour
+{
+    #region Public Variable
+    public GameObject LevelButtonPrefab;
+    //public CoinsRepository CoinRepo;
+    public AudioSource audioSrc;
+    public AudioClip clip;
+    #endregion
+
+    #region Private Variable
+    [SerializeField]
+    private LevelRepository levelRepo;
+    #endregion
+
+    #region Public Method
+    public void MainMenu()
+    {
+        //CoinRepo.Save();
+        audioSrc.PlayOneShot(clip);
+        SceneManager.LoadScene("MainMenu");
+        //Debug.Log("coinRepo" + CoinRepo.Get());
+    }
+    #endregion
+
+    #region Private Method
+    private void Awake()
+    {
+        //CoinRepo.Save();
+    }
+    private void Start()
+    {
+        bool[] levels = levelRepo.RetriveAllLevel();
+        for (int i = 0; i < levelRepo.LevelCount; i++)
+        {
+            GameObject btn = Instantiate(LevelButtonPrefab, transform.position, Quaternion.identity) as GameObject;
+            btn.transform.SetParent(transform);
+            btn.transform.localScale = Vector3.one;
+            btn.GetComponent<ButtonLevelController>().Init(i, levels[i]);
+        }
+    }
+    private void OnApplicationQuit()
+    {
+        //CoinRepo.Save();
+    }
+    #endregion
+}
